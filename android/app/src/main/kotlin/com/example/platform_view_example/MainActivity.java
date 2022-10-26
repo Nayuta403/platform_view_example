b41aeaa6148f67898ebf95a8b5d666a491b47e14
nayuta <dujunda@bytedance.com>
2022-10-26 17:19:38 +0800

    init

--- android/app/src/main/kotlin/com/example/platform_view_example/MainActivity.java
+++ android/app/src/main/kotlin/com/example/platform_view_example/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.platform_view_example;
 
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
@@ -9,7 +8,6 @@ import io.flutter.embedding.android.FlutterView;
 import io.flutter.embedding.engine.FlutterEngine;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends FlutterActivity {
   FlutterView containerView;
@@ -25,27 +23,27 @@ public class MainActivity extends FlutterActivity {
         .registerViewFactory("<platform-view-type>", new NativeViewFactory());
   }
   Handler handler=new Handler();
-  Runnable runnable=new Runnable() {
-    @Override
-    public void run() {
-      // TODO Auto-generated method stub
-      Log.e("DJD"," scheduleAtFixedRate 执行了 当前"+ need+" 转换为 ImageView");
-      if (need){
-        need = false;
-        containerView.renderToImageView();
-      }else{
-        need = true;
-        containerView.backToSurface();
-
-      }
-      handler.postDelayed(this, 10000);//表示第一次运行时的时间延迟
-    }
-  };
+  //Runnable runnable=new Runnable() {
+  //  @Override
+  //  public void run() {
+  //    // TODO Auto-generated method stub
+  //    Log.e("DJD"," scheduleAtFixedRate 执行了 当前"+ need+" 转换为 ImageView");
+  //    if (need){
+  //      need = false;
+  //      containerView.renderToImageView();
+  //    }else{
+  //      need = true;
+  //      containerView.backToSurface();
+  //
+  //    }
+  //    handler.postDelayed(this, 10000);//表示第一次运行时的时间延迟
+  //  }
+  //};
   public void setContentView(View view) {
     super.setContentView(view);
     containerView = (FlutterView) view;
 
-    handler.postDelayed(runnable, 10000);//每两秒执行一次runnable.
+    //handler.postDelayed(runnable, 10000);//每两秒执行一次runnable.
 
   }
 }
